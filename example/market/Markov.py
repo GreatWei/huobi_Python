7ed1cccedd2aadc61716292300bd5ba3d1d45f9f
GreatWei <1121412775@qq.com>
2021-05-08 00:20:32 +0800

    1

---
+++ example/market/Markov.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+# %是jupyter notebook的魔法命令
+# % matplotlib inline
+# 正常显示画图时出现的中文和负号
+from pylab import mpl
+
+mpl.rcParams['font.sans-serif'] = ['SimHei']
+mpl.rcParams['axes.unicode_minus'] = False
+
+# 获取上证综指数据
+import tushare as ts
+
+df = ts.get_k_data('sh', start='2000-01-01', end='2020-06-16')
+df.index = pd.to_datetime(df.date)
+df.head()
+
+# 上证综指周收益率
+df_ret = df.close.resample('W').last().pct_change().dropna()
+df_ret.plot(title='上证综指周收益率', figsize=(15, 4))
+# plt.show()
+
+
+# 平稳性检验
+# 使用arch包中的单位根检验unitroot导入ADF
+from arch.unitroot import ADF
+
+adf = ADF(df_ret)
+print(adf)
+
+# 模型拟合
+mod = sm.tsa.MarkovRegression(df_ret.dropna(),
+                              k_regimes=3, trend='nc', switching_variance=True)
+
+res = mod.fit()
+res.summary()
+
+fig, axes = plt.subplots(3, figsize=(12, 8))
+ax = axes[0]
+ax.plot(res.smoothed_marginal_probabilities[0])
+ax.set(title='上证综指低波动平滑概率图')
+ax = axes[1]
+ax.plot(res.smoothed_marginal_probabilities[1])
+ax.set(title='上证综指中波动平滑概率图')
+ax = axes[2]
+ax.plot(res.smoothed_marginal_probabilities[2])
+ax.set(title='上证综指高波动平滑概率图')
+
+
+# fig.tight_layout()
+
+
+def plot_rsm(code, title, start='2010-01-01', end='2020-06-17'):
+    df = ts.get_k_data(code, start=start, end=end)
+    df.index = pd.to_datetime(df.date)
+    df_ret = df.close.resample('w').last().pct_change().dropna()
+    # 模型拟合
+    print("df_ret.dropna()", df_ret.dropna())
+    mod = sm.tsa.MarkovRegression(df_ret.dropna(), k_regimes=3, trend='nc', switching_variance=True)
+    res = mod.fit()
+    print(res.predicted_marginal_probabilities)
+    print(res.smoothed_marginal_probabilities)
+    print(res.predict())
+    fig, axes = plt.subplots(4, figsize=(12, 8))
+    ax = axes[0]
+    ax.plot(res.smoothed_marginal_probabilities[0])
+    ax.set(title=title + '低波动平滑概率图')
+    ax = axes[1]
+    ax.plot(res.smoothed_marginal_probabilities[1])
+    ax.set(title=title + '中波动平滑概率图')
+    ax = axes[2]
+    ax.plot(res.smoothed_marginal_probabilities[2])
+    ax.set(title=title + '高波动平滑概率图')
+    # fig.tight_layout()
+    # ax = axes[3]
+    # a
+
+
+plot_rsm('002400', '省广集团')
+plt.show()
